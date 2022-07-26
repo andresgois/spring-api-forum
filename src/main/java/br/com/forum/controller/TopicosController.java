87795d9e87073d3acb92773ef9d9766b2d52aa92
Andre Gois <andresousalee@gmail.com>
2022-07-26 08:18:07 -0300

    :sparkles: feat: create repository topico

--- src/main/java/br/com/forum/controller/TopicosController.java
+++ src/main/java/br/com/forum/controller/TopicosController.java
@@ -3,21 +3,36 @@ package br.com.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.forum.controller.DTO.TopicoDTO;
 import br.com.forum.modelo.Curso;
 import br.com.forum.modelo.Topico;
+import br.com.forum.repository.TopicoRepository;
 
 //@Controller
 @RestController
 public class TopicosController {
+	
+	private TopicoRepository topicoRepository;
+	
+	@Autowired
+	public TopicosController(TopicoRepository topicoRepository) {
+		this.topicoRepository = topicoRepository;
+	}
+	
 	@RequestMapping("/topicos")
 	//@ResponseBody
 	public List<TopicoDTO> lista(){
-		Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
+		/**
+		 * Vesão antiga
+		 * Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
+		 * return TopicoDTO.converter(Arrays.asList(topico, topico, topico));
+		 */
+		List<Topico> topicos = topicoRepository.findAll();
+		return TopicoDTO.converter(topicos);
 		
-		return TopicoDTO.converter(Arrays.asList(topico, topico, topico));
 	}
 }
