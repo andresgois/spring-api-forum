608982a232d901fa30b1c957c0896d5923eb6fca
Andre Gois <andresousalee@gmail.com>
2022-07-29 18:13:01 -0300

    :sparkles: feat: authenticate user

--- src/main/java/br/com/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/forum/config/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package br.com.forum.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,16 +8,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private AutenticacaoService autenticacaoService;
+	
 	// Configuração de autenticação | Login
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// TODO Auto-generated method stub
 		//super.configure(auth);
+		auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());;
 	}
 	
 	// Configurações de autorização | liberação de URL's
@@ -34,5 +40,9 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	public void configure(WebSecurity web) throws Exception {
 		// TODO Auto-generated method stub
 		//super.configure(web);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(new BCryptPasswordEncoder().encode("123456"));
 	}
 }
