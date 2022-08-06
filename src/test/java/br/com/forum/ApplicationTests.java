9e74175601c0d62eb5afb8ab66c2fad75b4620c2
Andre Gois <andresousalee@gmail.com>
2022-08-05 21:26:59 -0300

    :bug:	fix: aprroved test

--- src/test/java/br/com/forum/ApplicationTests.java
+++ src/test/java/br/com/forum/ApplicationTests.java
@@ -1,13 +1,17 @@
 package br.com.forum;
 
 import org.junit.Assert;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@ExtendWith(SpringExtension.class)
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
 @SpringBootTest
+@AutoConfigureMockMvc
 class ApplicationTests {
 
 	@Test
