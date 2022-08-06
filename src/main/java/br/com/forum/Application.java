89936dfa1c261580710eb56470a2c4a6dd921a0a
Andre Gois <andresousalee@gmail.com>
2022-08-05 21:27:58 -0300

    :sparkles:	feat: configure to war

--- src/main/java/br/com/forum/Application.java
+++ src/main/java/br/com/forum/Application.java
@@ -2,6 +2,8 @@ package br.com.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
@@ -11,10 +13,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSpringDataWebSupport // Pegar dados de páginação do Pageable
 @EnableCaching				// habilita o uso de cache
 @EnableSwagger2
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	// Tudo isso para gerar um war
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
 }
